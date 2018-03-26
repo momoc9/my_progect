15a2344820c6ab073d6547311eec26c54bdfb38c
chenlili <664740478@qq.com>
2018-03-26 20:52:59 +0800

    vue

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import axios from 'axios'
+// Vue.prototype.$http = axios //   axios并不是vue插件,所以只能在每个需要发送请求的组件中即时引入。为了解决这个问题，我们在引入 axios 之后，通过修改原型链，来更方便的使用。
+
+// 引用API文件
+import api from './api/index.js'
+// 将API方法绑定到全局
+Vue.prototype.$api = api
 
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
